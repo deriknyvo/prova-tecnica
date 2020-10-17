74f141c1c033f0c16ad629b0e09f4d4135b8f0e4
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2020-10-17 14:22:00 -0300

    Concluindo validação html

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    console.log(event.target);
+    const [ano, mes, dia] = event.target.elements[2].value.split('-');
+    const hoje = new Date();
+    const dataNascimento = new Date(ano, mes, dia, 0, 0, 0);
+    const tempoParaTeste = 1000 * 60 * 60 * 24 * 365 * 18;
+    const span = document.getElementById('menor');
+
+    if (hoje.getTime() - dataNascimento.getTime() >= tempoParaTeste) {
+        span.style.display = 'none';
+    } else {
+        span.style.display = 'block';
+    }
 });
